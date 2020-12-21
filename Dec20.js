6c2ab274c8d39772168169cb41f29ecfe283d638
Daniel Flores <danielflores125@gmail.com>
2020-12-20 22:29:56 -0800

    Reverse link list

---
+++ Dec20.js
@@ -0,0 +1,12 @@
+let reverseList = function(head) {
+  let prevNode = null;
+
+  while (head !== null) {
+      let nextNode = head.next;
+      head.next = prevNode;
+      prevNode = head;
+      head = nextNode;
+  }
+
+  return prevNode;
+};
